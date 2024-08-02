7f94b09c8a85ca25b6a69a54b3a7e1ef5004bc7d
CamHirsch23 <hirschboeck13@gmail.com>
2024-08-02 18:28:40 -0500

    Create HomepageRenderApp.js

---
+++ HomepageRenderApp.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import HomePage from './HomePage'; // Make sure the path to your HomePage component is correct
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
